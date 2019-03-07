ff325d62da15ff99c319421055ddc4956d8dbb33
Andy Gocke <angocke@microsoft.com>
2019-03-06 23:38:29 -0800

    WIP

--- src/Tools/CompilerBenchmarks/Program.cs
+++ src/Tools/CompilerBenchmarks/Program.cs
@@ -5,7 +5,11 @@ using System.IO;
 using System.Linq;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Validators;
 
@@ -13,13 +17,62 @@ namespace CompilerBenchmarks
 {
     public class Program
     {
+        private class ValueOnlyExporter : CsvMeasurementsExporter
+        {
+            public ValueOnlyExporter() : base(CsvSeparator.Comma)
+            { }
+
+            public override void ExportToLog(Summary summary, ILogger logger)
+            {
+                string realSeparator = Separator;
+                var columns = new[] {
+                    new MeasurementColumn("Target", (summary, report, m) => report.BenchmarkCase.Descriptor.Type.Name + "." + report.BenchmarkCase.Descriptor.WorkloadMethodDisplayInfo)
+                };
+                logger.WriteLine(string.Join(realSeparator, columns.Select(c => CsvHelper.Escape(c.Title, realSeparator))));
+
+                foreach (var report in summary.Reports)
+                {
+                    foreach (var measurement in report.AllMeasurements)
+                    {
+                        for (int i = 0; i < columns.Length;)
+                        {
+                            logger.Write(CsvHelper.Escape(columns[i].GetValue(summary, report, measurement), realSeparator));
+
+                            if (++i < columns.Length)
+                            {
+                                logger.Write(realSeparator);
+                            }
+                        }
+                        logger.WriteLine();
+                    }
+                }
+            }
+
+            private struct MeasurementColumn
+            {
+                public string Title { get; }
+                public Func<Summary, BenchmarkReport, Measurement, string> GetValue { get; }
+
+                public MeasurementColumn(string title, Func<Summary, BenchmarkReport, Measurement, string> getValue)
+                {
+                    Title = title;
+                    GetValue = getValue;
+                }
+            }
+
+            public static new ValueOnlyExporter Default = new ValueOnlyExporter();
+        }
+
         private class IgnoreReleaseOnly : ManualConfig
         {
             public IgnoreReleaseOnly()
             {
                 Add(JitOptimizationsValidator.DontFailOnError);
                 Add(DefaultConfig.Instance.GetLoggers().ToArray());
-                Add(DefaultConfig.Instance.GetExporters().ToArray());
+                Add(DefaultConfig.Instance
+                    .GetExporters()
+                    .Concat(new[] { ValueOnlyExporter.Default })
+                    .ToArray());
                 Add(DefaultConfig.Instance.GetColumnProviders().ToArray());
                 Add(MemoryDiagnoser.Default);
                 Add(Job.Core.WithGcServer(true));
