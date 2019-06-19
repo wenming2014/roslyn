0f1e1040f41486ecfab5e492143c50b6dfadfd8b
Andy Gocke <angocke@microsoft.com>
2019-06-19 12:38:01 -0700

    Use local scriptroot

--- eng/common/tools.sh
+++ eng/common/tools.sh
@@ -4,13 +4,13 @@ source="${BASH_SOURCE[0]}"
 
 # resolve $source until the file is no longer a symlink
 while [[ -h "$source" ]]; do
-  scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+  local scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
   source="$(readlink "$source")"
   # if $source was a relative symlink, we need to resolve it relative to the path where the
   # symlink file was located
   [[ $source != /* ]] && source="$scriptroot/$source"
 done
-scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
+local scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
 # Initialize variables if they aren't already defined.
 
